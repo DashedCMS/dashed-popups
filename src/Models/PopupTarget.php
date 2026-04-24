9fe3eec12d6f39579dcd5caac0237bb8361593f4
Robin van Maasakker <robin@dashed.nl>
2026-04-24 11:05:58 +0200

    feat: add PopupTarget model with rule/match type constants

---
+++ src/Models/PopupTarget.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Dashed\DashedPopups\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class PopupTarget extends Model
+{
+    protected $table = 'dashed__popup_targets';
+
+    public const RULE_INCLUDE = 'include';
+    public const RULE_EXCLUDE = 'exclude';
+
+    public const MATCH_URL_PATTERN = 'url_pattern';
+    public const MATCH_ALL_OF_TYPE = 'all_of_type';
+    public const MATCH_SPECIFIC_MODEL = 'specific_model';
+
+    protected $fillable = [
+        'popup_id',
+        'rule_type',
+        'match_type',
+        'pattern',
+        'targetable_type',
+        'targetable_id',
+    ];
+
+    public function popup(): BelongsTo
+    {
+        return $this->belongsTo(Popup::class);
+    }
+
+    public function targetable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function scopeIncludes($query)
+    {
+        return $query->where('rule_type', self::RULE_INCLUDE);
+    }
+
+    public function scopeExcludes($query)
+    {
+        return $query->where('rule_type', self::RULE_EXCLUDE);
+    }
+}
